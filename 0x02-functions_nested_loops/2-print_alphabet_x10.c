d9986ea8e6db5e7047d63639e53b965f159cd2f5
SEA471 <seallen422@mail.com>
2021-06-30 15:50:48 +0000

    Corrected file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 	int a;
 	int count = 10;
 	/*Loop string 10 time */
-	for (count=0; count<10; count++)
+	for (count = 0; count < 10; count++)
 	{
-		for (i=97; i<= 122; i++)
-		_putchar(i);
+		for (a = 97; a <= 122; a++)
+		_putchar(a);
 		_putchar('\n');
 	}
 
