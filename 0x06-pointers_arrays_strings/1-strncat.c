db053909d5eb8c95f005a9b01543dcbedbe905d0
SEA471 <seallen422@mail.com>
2022-02-14 05:19:46 +0000

    Add function and file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ **_strncat - creates a new string by combining two
+ *
+ *@dest: string destination
+ *
+ *@src:  string source
+ *
+ *@n: number of characters to append
+ *
+ *Return: 0 = Success
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+char *d[100];
+char *s[100];
+if (n != 0)
+n++;
+{
+if (d != '\0')
+return (dest);
+}
+{
+if (s != '\0')
+return (src);
+}
+{
+return (0);
+}
+}
