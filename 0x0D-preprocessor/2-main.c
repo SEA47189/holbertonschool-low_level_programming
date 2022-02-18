35e2bace7634af6e73c93a6863cc9baec636bcc7
SEA471 <seallen422@mail.com>
2022-02-18 05:17:31 +0000

    Change to function parameter

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
-*main - entry point
-*
-*function - prints filenme followed
-*by a new line
-*
-*printf: prints filename
-*
-*@argc: counter points to array
-*@argv: array of counter
-*
-*Return: 0 = success
-*/
+ *main - entry point
+ *
+ *function - prints filenme followed
+ *by a new line
+ *
+ *printf: prints filename
+ *
+ *@argc: counter points to array
+ *@argv: array of counter
+ *
+ *Return: 0 = success
+ */
 int main(int argc, char **argv)
 {
 int a;
 
-for (a = 0; a < argc; a++)
+for (a = 1; a < argc; a++)
 {
 printf("%s\n", argv[0]);
 }
 {
-return (0);
+return (a);
 }
 }
