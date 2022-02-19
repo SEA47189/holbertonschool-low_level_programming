a0d45e5b73d9395d35ebb50cbe905ada7f05bb0f
SEA471 <seallen422@mail.com>
2022-02-19 01:33:56 +0000

    Update to function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,30 +16,28 @@ char *str_concat(char *s1, char *s2)
 int len = 0;
 char *a;
 char *b;
-{
+
 if (s1 == NULL)
 return (NULL);
 
 if (s2 == NULL)
 return (NULL);
-{
+
 while (s1[len])
-len++;
-a = malloc(len + 1);
-}
 {
+len++;
+}
+a = malloc(len + 1);
+
 while (s2[len])
+{
 len++;
 b = malloc(len + 1);
+}
 {
 a[len] = b[len];
 len++;
 }
 b[len] = '\0';
 return (b);
-{
-return (0);
-}
-}
-}
 }
