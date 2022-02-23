2de131c9d00d3c8668d11c95dc298b0e31a12dec
SEA471 <seallen422@mail.com>
2022-02-23 04:39:59 +0000

    Add struct and filename

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - newly defined structure for dogs
+ * @a: name
+ * @b: age
+ * @c: owner
+ * Description: struct to give ouput of dogs info
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
